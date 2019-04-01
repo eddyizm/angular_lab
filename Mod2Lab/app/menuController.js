66cdf28e89b6414a4ba45b86e39b5d83cb3203e8
eddyizm <eddyizm@gmail.com>
2019-03-31 21:34:21 -0700

    added menu controller and ng model objects to page.

--- Mod2Lab/app/menuController.js
+++ Mod2Lab/app/menuController.js
@@ -2,6 +2,17 @@ app.controller("menuController", [
   '$scope',
   function($scope){
     $scope.model = { title: 'Our Menu'};
+    
+    $scope.changeMainDish = function (item){
+      $scope.model.mainDish = item;
+    }
+    
+    $scope.$watch('model.mainDish', function (newValue, oldValue){
+      if (newValue === 'BBQ Chicken Pizza'){
+        alert('You have selected the BBQ Chicken Pizza Bitch!');
+      }
+    });
+
     $scope.changeMainDish = function (item){
       $scope.model.mainDish = item;
     }
