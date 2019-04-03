d0d296405e92c91ae8243ef75aff20afc03be277
eddyizm <eddyizm@gmail.com>
2019-04-02 21:32:21 -0700

    fixed controller to match assignment requirements.

--- Mod2Lab/app/menuController.js
+++ Mod2Lab/app/menuController.js
@@ -2,13 +2,22 @@ app.controller("menuController", [
   '$scope',
   function($scope){
     $scope.model = { title: 'Our Menu'};
-    $scope.model = { price: ''};
-
-    $scope.changeMainDish = function (item, price){
-      $scope.model.mainDish = item;
-      $scope.model.price = price;
-    }
     
+    $scope.model.mainDish = {
+      item: '', 
+      price: ''
+    };
+   
+    $scope.changeMainDish = function (item, price){
+      
+      $scope.model.mainDish.item = item;
+      // console.log(item)
+      $scope.model.mainDish.price = price;
+      //console.log(price)
+      // console.log($scope.model.mainDish.item)
+      // console.log($scope.mainDish.price)
+    }
+   
     $scope.$watch('model.mainDish', function (newValue, oldValue){
       if (newValue === 'BBQ Chicken Pizza'){
         alert('You have selected the BBQ Chicken Pizza Bitch!');
